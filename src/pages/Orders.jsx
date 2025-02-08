1dd8cf141658c8e2d04144ed1312d65dc157182e
chidera <chideranwokoye555@gmail.com>
2025-02-08 22:05:26 +0100

    adding orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,32 @@
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+
 const Orders = () => {
-  return <div></div>;
+  const { products, currency } = useContext(ShopContext);
+  return (
+    <div className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1={"MY"} text2={"ORDERS"} />
+      </div>
+      <div className="">
+        {products.slice(1, 4).map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col py-4 border-t border-b text-gray-700 md:flex-row md:items-center md:justify-between gap-4"
+          >
+            <div className="flex items-start gap-6 text-sm">
+              <img
+                className="w-16 sm:w-20"
+                src={item.image[0]}
+                alt="item-img"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
