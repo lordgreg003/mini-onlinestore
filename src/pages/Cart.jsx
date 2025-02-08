2d02fe9692016f3cf8b7a284cdcf009652fd80c8
chidera <chideranwokoye555@gmail.com>
2025-02-08 18:22:14 +0100

    added delete icon

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { useState } from "react";
 import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Cart = () => {
   const { products, currency, cartItems } = useContext(ShopContext);
@@ -46,8 +47,24 @@ const Cart = () => {
                   <p className="text-xs sm:text-lg font-medium">
                     {productData.name}
                   </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                      {item.size}
+                    </p>
+                  </div>
                 </div>
               </div>
+              <input
+                className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 "
+                type="number"
+                min={1}
+                defaultValue={item.quantity}
+              />
+              <img className="w-4 mr-4 sm:w-5 cursor-pointer" src={assets.bin_icon} alt="bin-icon" />
             </div>
           );
         })}
